09c53f6124f08869b9d5bc4692313f495840a753
Vladimir Malits <vladimir.malits@gmail.com>
2020-12-23 20:30:21 +0200

    refactoring

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -1,10 +1,9 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Models\Topic;
 use App\Repositories\Contracts\TopicRepository;
-use Illuminate\Http\Request;
 
 class TopicController extends Controller
 {
@@ -14,6 +13,6 @@ class TopicController extends Controller
 
     public function index()
     {
-        dd($this->topicRepository->all());
+        return $this->topicRepository->all();
     }
 }
